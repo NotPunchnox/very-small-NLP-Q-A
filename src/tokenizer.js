a555851d185b10d61ef16ca9a047ab9dfba9440e
NotPunchnox <punchnoxpro@gmail.com>
2025-04-25 01:07:47 +0200

    add class for tokenizer

---
+++ src/tokenizer.js
@@ -0,0 +1,93 @@
+/* Very Small SML */
+
+// Training Data ( small dataset )
+const text = [
+  "Bonjour je suis un robot",
+  "Un robot est une machine",
+  "Il fait beau dehors"
+]
+
+class Tokenizer {
+
+  const vocab = [];
+  const pairs = [];
+
+  constructor() {
+
+  }
+
+  static init() {
+
+    // Make the tokenizer and build pairs
+    for (const line of text) {
+
+      for (const word of line.split(' ')) {
+        if (!Object.keys(vocab).includes(word)) {
+          let object = { [vocab.length]: word };
+
+          vocab.push(object);
+
+          if (vocab.length % 2 == 0) {
+            pairs.push([Object.keys(object)[0] - 1, Object.keys(object)[0]]);
+          }
+        }
+      }
+
+    }
+
+  }
+}
+
+// Make the tokenizer and build pairs
+for (const line of text) {
+
+  for (const word of line.split(' ')) {
+    if (!Object.keys(vocab).includes(word)) {
+
+      let object = { [vocab.length]: word }
+
+      vocab.push(object);
+      if (vocab.length % 2 == 0) {
+        pairs.push([Object.keys(object)[0] - 1, Object.keys(object)[0]])
+      }
+    }
+  }
+
+}
+
+
+// Function for tokenize text
+function Tokenize(text) {
+  const result = [];
+  let text_parsed = text.split(' ');
+
+  for (const word of text_parsed) {
+    const token = Object.keys(vocab.find(a => a[Object.keys(a)[0]] == word))[0];
+    result.push(token);
+  }
+
+  return result;
+}
+
+// Function for decode tokenized text
+function Decode(tokens) {
+  const result = [];
+
+  for (const token of tokens) {
+    const word = vocab[token][token];
+    result.push(word);
+  }
+
+  return result;
+}
+
+
+// Tokenize text
+const output = Tokenize("Bonjour je");
+console.log(output);
+
+// Decode tokenized text
+const decoded_result = Decode(output);
+console.log("decoded result:", decoded_result);
+
+
