65ef6f399f363e4163eb666b9d92975ccf8b63ea
NotPunchnox <punchnoxpro@gmail.com>
2025-04-25 14:28:33 +0200

    Class Tokenizer : OK

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const { Tokenizer } = require('./src/tokenizer.js');
 const tokenizer = new Tokenizer();
 
 tokenizer.init();
+
+// Tokenize example text
+const tokens = tokenizer.tokenize("Bonjour je suis samuel");
+
+
--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,78 +1,69 @@
-/* Very Small SML */
-
-// Training Data ( small dataset )
+// Training Data (small dataset)
 const text = [
   "Bonjour je suis un robot",
   "Un robot est une machine",
   "Il fait beau dehors"
-]
+];
 
 class Tokenizer {
+  vocab = [];
+  pairs = [];
 
-  vocab = []
-  pairs = []
+  constructor() { }
 
-  constructor() {
-
-  }
-
-  static init() {
-
-    // Make the tokenizer and build pairs
+  init() {
+    // Build vocabulary and pairs
     for (const line of text) {
+      const words = line.split(' ');
 
-      for (const word of line.split(' ')) {
-        if (!Object.keys(this.vocab).includes(word)) {
-          let object = { [this.vocab.length]: word };
-
-          this.vocab.push(object);
-
-          if (this.vocab.length % 2 == 0) {
-            this.pairs.push([Object.keys(object)[0] - 1, Object.keys(object)[0]]);
-          }
+      // Add words to vocab
+      for (const word of words) {
+        if (!this.vocab.includes(word)) {
+          this.vocab.push(word);
         }
       }
 
+      // Create bigram pairs from words
+      for (let i = 0; i < words.length - 1; i++) {
+        const pair = [this.vocab.indexOf(words[i]), this.vocab.indexOf(words[i + 1])];
+        this.pairs.push(pair);
+      }
     }
-
   }
 
-  static tokenize(text) {
+  tokenize(text) {
     const result = [];
-    let text_parsed = text.split(' ');
+    const text_parsed = text.split(' ');
+
+    //console.log('vocab:', this.vocab);
+    console.log('pairs:', this.pairs);
 
     for (const word of text_parsed) {
-      const token = Object.keys(vocab.find(a => a[Object.keys(a)[0] == word]))[0];
-      result.push(token);
+      const token = this.vocab.indexOf(word);
+
+      // If token not exist in vocabulary
+      if (token === -1) {
+        result.push('<unk>');
+      } else result.push(token);
+
     }
 
     return result;
   }
 
-  static decode(tokens) {
+  decode(tokens) {
     const result = [];
 
     for (const token of tokens) {
-      const word = vocab[token][token];
-      result.push(word);
+      if (token < 0 || token >= this.vocab.length) {
+        console.warn(`Invalid token "${token}". Skipping.`);
+        continue;
+      }
+      result.push(this.vocab[token]);
     }
 
     return result;
-
   }
-
 }
-
-
-/*
-// Tokenize text
-const output = Tokenize("Bonjour je");
-console.log(output);
-
-// Decode tokenized text
-const decoded_result = Decode(output);
-console.log("decoded result:", decoded_result);
-*/
-
 
 module.exports = { Tokenizer };
