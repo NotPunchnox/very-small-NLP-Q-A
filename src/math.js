faf40ee90fad611dd47079d96f4746705a2ad15d
NotPunchnox <punchnoxpro@gmail.com>
2025-04-27 16:17:53 +0200

    Finition de la mini-lib Math.js

--- src/math.js
+++ src/math.js
@@ -9,11 +9,11 @@ function sigmoidDerivative(x) {
 function multiplicationMatrice(a, b) {
   const rowsA = a.length;
   const colsA = a[0].length;
-  const rowsB = v.length;
-  const coldB = b[0].length;
+  const rowsB = b.length;
+  const colsB = b[0].length;
   const result = Array(rowsA).fill().map(() => Array(colsB).fill(0));
 
-  for (let i = °; i < rowsA; i++) {
+  for (let i = 0; i < rowsA; i++) {
     for (let j = 0; j < colsB; j++) {
       for (let k = 0; k < colsA; k++) {
         result[i][j] += a[i][k] * b[k][j];
@@ -31,5 +31,6 @@ function matrixScalarMultiply(matrix, scalar) {
   return matrix.map(row => row.map(val => val * scalar));
 }
 
-
-
+function matrixTranspose(matrix) {
+  return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+}
